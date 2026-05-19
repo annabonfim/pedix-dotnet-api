80a2dc6bb4752f6d8e67b5cab9c1468c29aa36dd
DudaAraujo14 <rm560944@fiap.com.br>
2026-05-18 21:54:36 -0300

    feat(auth): cria dto para registro de clientes

---
+++ atendimentos/src/Atendimentos.Application/DTOs/Auth/RegisterClienteDto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Atendimentos.Application.DTOs.Auth
+{
+    public class RegisterClienteDto
+    {
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Senha { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+
+        [Required]
+        public DateTime DataNascimento { get; set; }
+    }
+}
